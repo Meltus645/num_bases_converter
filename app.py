64881a861f98713e765da3db47175461a36e7816
=wajuma <=meltusjuma5@gmail.com>
2021-07-13 23:44:58 +0300

    add ui

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from kivymd.uix.screen import MDScreen
 from kivymd.app import MDApp
 from kivy.uix.image import Image
-from kivymd.uix.button import MDFillRoundFlatIconButton,MDFillRoundFlatButton
+from kivymd.uix.button import MDFillRoundFlatIconButton,MDRectangleFlatButton
 from kivymd.uix.textfield import MDTextField
 from kivymd.uix.label import MDLabel
 from kivymd.uix.toolbar import MDToolbar
@@ -13,33 +13,42 @@ class MainApp(MDApp):
             self.toolbar.title ="Decimal to Binary"
             self.input_label.text ="Enter a decimal number"
             self.converted.text =""
+            self.input.text =''
             self.label.text =""
         else:
             self.state =0
             self.toolbar.title ="Binary to Decimal"
             self.input_label.text ="Enter a binary number"
             self.converted.text =""
+            self.input.text =''
             self.label.text =""
             
     def convert(self, args):
         if self.input.text =="":
             self.error_label.font_size =14
-            self.error_label.text ='this field is required!'
+            self.error_label.text ='This field is required!'
         else:
             if self.state ==0:
                 val =int(self.input.text, 2)
                 self.converted.text =str(val)
                 self.error_label.text =''
-                self.label.text ='in decimal is:'
+                self.label.text ='In Decimal is:'
             else:
                 val =bin(int(self.input.text))[2:]
                 self.converted.text =val
                 self.error_label.text =''
-                self.label.text ='in binary is:'
+                self.label.text ='In Binary is:'
             
     def build(self):
         self.state =0
         self.title ="Number Bases Converter"
+        self.theme_cls.font_styles["JetBrainsMono"] = [
+            "JetBrainsMono",
+            16,
+            False,
+            0.15,
+        ]
+        self.theme_cls.primary_hue = "600"
         screen =MDScreen() 
         """ ui widgets """
         # toolbar
@@ -61,7 +70,8 @@ class MainApp(MDApp):
             text ='Enter a binary number',
             halign ='left',
             pos_hint ={'center_x': 0.6, 'center_y': 0.55},
-            theme_text_color ="Secondary"
+            theme_text_color ="Secondary",
+            font_size =15
         )
         screen.add_widget(self.input_label)
         self.input =MDTextField(
@@ -69,7 +79,7 @@ class MainApp(MDApp):
             halign ='left',
             size_hint =(0.8, 1),
             pos_hint ={'center_x': 0.5, 'center_y': 0.5},
-            font_size =22
+            font_size =15
         )
         screen.add_widget(self.input)
         self.error_label =MDLabel(
@@ -81,13 +91,13 @@ class MainApp(MDApp):
         screen.add_widget(self.error_label)
         self.label =MDLabel(
             halign ='center',
-            pos_hint ={'center_x': 0.5, 'center_y': 0.35},
+            pos_hint ={'center_x': 0.5, 'center_y': 0.42},
             theme_text_color ="Secondary"
         )
         # output
         self.converted =MDLabel(
             halign ='center',
-            pos_hint ={'center_x': 0.5, 'center_y': 0.3},
+            pos_hint ={'center_x': 0.5, 'center_y': 0.37},
             theme_text_color ="Primary",
             font_style ="H5"
         )
@@ -95,10 +105,10 @@ class MainApp(MDApp):
         screen.add_widget(self.converted)
         
         # convert button
-        screen.add_widget(MDFillRoundFlatButton(
+        screen.add_widget(MDRectangleFlatButton(
                 text ="Convert",
                 font_size =17,
-                pos_hint ={'center_x': 0.5, 'center_y': 0.15},
+                pos_hint ={'center_x': 0.5, 'center_y': 0.25},
                 on_press =self.convert
             )
         )
