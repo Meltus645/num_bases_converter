d30497dc10bc4ea67f2299f288717b6c714ea44c
=wajuma <=meltusjuma5@gmail.com>
2021-07-13 22:41:40 +0300

    check if input field is empty

--- app.py
+++ app.py
@@ -8,14 +8,32 @@ from kivymd.uix.toolbar import MDToolbar
 
 class MainApp(MDApp):
     def flip(self):
-        print('flipping ...')
+        if self.state ==0:
+            self.state =1
+            self.toolbar.title ="Decimal to Binary"
+            self.input.text ="enter decimal number"
+            self.converted.text =""
+            self.label.text =""
+        else:
+            self.state =0
+            self.toolbar.title ="Binary to Decimal"
+            self.input.text ="enter binary number"
+            self.converted.text =""
+            self.label.text =""
+            
     def convert(self, args):
-        val =int(self.input.text, 2)
-        self.converted.text =str(val)
-        self.label.text ='in decimal is:'
+        if self.input.text !="":
+            val =int(self.input.text, 2)
+            self.converted.text =str(val)
+            self.input_label.text =''
+            self.label.text ='in decimal is:'
+        else:
+            self.input_label.font_size =14
+            self.input_label.text ='this field is required!'
+            
     def build(self):
-        screen =MDScreen()
-        
+        self.state =0
+        screen =MDScreen() 
         """ ui widgets """
         # toolbar
         self.toolbar =MDToolbar(title ="Binary to Decimal")
@@ -33,13 +51,20 @@ class MainApp(MDApp):
         )
         # user input
         self.input =MDTextField(
-            text ='enter a binary number',
+            text ='',
             halign ='center',
             size_hint =(0.8, 1),
             pos_hint ={'center_x': 0.5, 'center_y': 0.5},
             font_size =22
         )
         screen.add_widget(self.input)
+        self.input_label =MDLabel(
+            text ='enter a binary number',
+            halign ='center',
+            pos_hint ={'center_x': 0.5, 'center_y': 0.45},
+            theme_text_color ="Secondary"
+        )
+        screen.add_widget(self.input_label)
         self.label =MDLabel(
             halign ='center',
             pos_hint ={'center_x': 0.5, 'center_y': 0.35},
