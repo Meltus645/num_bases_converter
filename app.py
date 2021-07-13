3be70fccc01dc6aa6e4954a39172163ae75585dc
=wajuma <=meltusjuma5@gmail.com>
2021-07-13 22:15:56 +0300

    convert button

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ from kivymd.uix.toolbar import MDToolbar
 class MainApp(MDApp):
     def flip(self):
         print('flipping ...')
+    def convert(self, args):
+        val =int(self.input.text, 2)
     def build(self):
         screen =MDScreen()
         
         """ ui widgets """
         # toolbar
-        self.toolbar =MDToolbar(title ="Bin2Dec Converter")
+        self.toolbar =MDToolbar(title ="Binary to Decimal")
         self.toolbar.pos_hint ={"top": 1}
         self.toolbar.right_action_items =[ 
             ["rotate-3d-variant",lambda x: self.flip()]
@@ -32,7 +34,7 @@ class MainApp(MDApp):
             text ='enter a binary number',
             halign ='center',
             size_hint =(0.8, 1),
-            pos_hint ={'center_x': 0.5, 'center_y': 0.45},
+            pos_hint ={'center_x': 0.5, 'center_y': 0.5},
             font_size =22
         )
         screen.add_widget(self.input)
@@ -42,7 +44,25 @@ class MainApp(MDApp):
             pos_hint ={'center_x': 0.5, 'center_y': 0.35},
             theme_text_color ="Secondary"
         )
+        # output
+        self.converted =MDLabel(
+            text ='888',
+            halign ='center',
+            pos_hint ={'center_x': 0.5, 'center_y': 0.3},
+            theme_text_color ="Primary",
+            font_style ="H5"
+        )
         screen.add_widget(self.label)
+        screen.add_widget(self.converted)
+        
+        # convert button
+        screen.add_widget(MDFillRoundFlatButton(
+                text ="Convert",
+                font_size =17,
+                pos_hint ={'center_x': 0.5, 'center_y': 0.15},
+                on_press =self.convert
+            )
+        )
         return screen
 
 if __name__ == '__main__':
